d0692c4cf03a4771b9426c7262bbbe617e064305
peter <chuckaboxpeter@gmail.com>
2026-01-07 22:31:15 +1100

    feat: added dark background

---
+++ lib/app/app.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'flutter-wordle',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(scaffoldBackgroundColor: Colors.black),
+    );
+  }
+}
