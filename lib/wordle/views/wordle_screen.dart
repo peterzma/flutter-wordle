15278dcf81fa850864734db2486a7961a7e6e06d
peter <chuckaboxpeter@gmail.com>
2026-01-08 13:07:07 +1100

    feat: added word indexing

--- lib/wordle/views/wordle_screen.dart
+++ lib/wordle/views/wordle_screen.dart
@@ -11,11 +11,6 @@ class WordleScreen extends StatefulWidget {
 
 class _WordleScreenState extends State<WordleScreen> {
   GameStatus _gameStatus = GameStatus.playing;
-
-  final List<Word> _board = List.generate(
-    6,
-    (_) => Word(letters: List.generate(5, (_) => Letter.empty())),
-  );
   
   @override
   Widget build(BuildContext context) {
