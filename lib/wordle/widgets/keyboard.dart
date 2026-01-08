069197792094c582c37e5d69ae40ef7fddc58d7f
peter <chuckaboxpeter@gmail.com>
2026-01-08 14:30:11 +1100

    feat: added keyboard class

--- lib/wordle/widgets/keyboard.dart
+++ lib/wordle/widgets/keyboard.dart
@@ -1,5 +1,18 @@
+import 'package:flutter/material.dart';
+
 const _qwerty = [
   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
   ['ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'DEL'],
 ];
+
+class Keyboard extends StatelessWidget {
+  const Keyboard({ Key? key }): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
