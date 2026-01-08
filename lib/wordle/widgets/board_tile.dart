d380fcf436f5262c380f287130de09b050f420df
peter <chuckaboxpeter@gmail.com>
2026-01-08 14:03:48 +1100

    fix: wrong import

--- lib/wordle/widgets/board_tile.dart
+++ lib/wordle/widgets/board_tile.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_wordle/app/wordle_app.dart';
+import 'package:flutter_wordle/wordle/wordle.dart';
 
 class BoardTile extends StatelessWidget {
   const BoardTile({
