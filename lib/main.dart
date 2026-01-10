1cd0acae4a098eb6ef6f9ea5bf2017f8bda0db4b
peter <chuckaboxpeter@gmail.com>
2026-01-10 11:55:37 +1100

    feat: added min/max window size

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_wordle/app/app.dart';
+import 'package:window_size/window_size.dart';
 
-void main() => runApp(const App());
+void main() { 
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if (Platform.isWindows || Platform.isLinux || Platform.isMacOS) {
+    setWindowTitle('Flutter Wordle');
+    setWindowMinSize(const Size(400, 600));
+  }
+  
+  runApp(const App()); 
+}
