2666746a84df6335ce381b1f1708be06fb6bb4ef
peter <chuckaboxpeter@gmail.com>
2026-01-13 22:43:58 +1100

    feat: added home screen

---
+++ lib/app/home_screen.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_wordle/wordle/views/wordle_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+}
