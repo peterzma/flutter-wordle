3b425a0ba93f538cb79d6ecca4b83653b64b18bf
peter <chuckaboxpeter@gmail.com>
2026-01-15 23:59:51 +1100

    feat: added hover border effect

--- lib/home/widgets/subject_grid.dart
+++ lib/home/widgets/subject_grid.dart
@@ -48,6 +48,7 @@ class _AnimatedSubjectTile extends StatefulWidget {
 
 class _AnimatedSubjectTileState extends State<_AnimatedSubjectTile> {
   double _scale = 1.0;
+  bool _hovering = false;
 
   void _onTapDown(_) => setState(() => _scale = 0.95);
   void _onTapUp(_) => setState(() => _scale = 1.0);
@@ -56,89 +57,95 @@ class _AnimatedSubjectTileState extends State<_AnimatedSubjectTile> {
   @override
   Widget build(BuildContext context) {
     final sub = widget.subject;
-    return GestureDetector(
-      onTap: widget.onTap,
-      onTapDown: _onTapDown,
-      onTapUp: _onTapUp,
-      onTapCancel: _onTapCancel,
-      child: AnimatedScale(
-        scale: _scale,
-        duration: const Duration(milliseconds: 100),
-        curve: Curves.easeOut,
-        child: Container(
-          padding: const EdgeInsets.all(12),
-          decoration: BoxDecoration(
-            color: const Color(0xFF1A1F2B),
-            borderRadius: BorderRadius.circular(16),
-            border: Border.all(
-              color: Colors.grey.withValues(alpha: 0.4),
-              width: 0.5,
-            ),
-          ),
-          child: Row(
-            children: [
-              Container(
-                width: 40,
-                height: 40,
-                alignment: Alignment.center,
-                decoration: BoxDecoration( 
-                color: Colors.white.withValues(alpha: 0.05),
-                borderRadius: BorderRadius.circular(12),
-                ),
-                child: Icon(
-                  _getIcon(sub.icon),
-                  color: sub.color ?? Colors.blue,
-                  size: 24,
-                ),
+    return MouseRegion(
+      onEnter: (_) => setState(() => _hovering = true),
+      onExit: (_) => setState(() => _hovering = false),
+      cursor: SystemMouseCursors.click,
+      child: GestureDetector(
+        onTap: widget.onTap,
+        onTapDown: _onTapDown,
+        onTapUp: _onTapUp,
+        onTapCancel: _onTapCancel,
+        child: AnimatedScale(
+          scale: _scale,
+          duration: const Duration(milliseconds: 100),
+          curve: Curves.easeOut,
+          child: AnimatedContainer(
+            duration: const Duration(milliseconds: 150),
+            padding: const EdgeInsets.all(12),
+            decoration: BoxDecoration(
+              color: const Color(0xFF1A1F2B),
+              borderRadius: BorderRadius.circular(16),
+              border: Border.all(
+                color: _hovering ? Colors.blueAccent : Colors.grey.withValues(alpha: 0.4),
+                width: _hovering ? 1.5 : 0.5,
               ),
-              const SizedBox(width: 12),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      sub.name,
-                      style: const TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.w600,
+            ),
+            child: Row(
+              children: [
+                Container(
+                  width: 40,
+                  height: 40,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration( 
+                  color: Colors.white.withValues(alpha: 0.05),
+                  borderRadius: BorderRadius.circular(12),
+                  ),
+                  child: Icon(
+                    _getIcon(sub.icon),
+                    color: sub.color ?? Colors.blue,
+                    size: 24,
+                  ),
+                ),
+                const SizedBox(width: 12),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        sub.name,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
-                    ),
-                    if (sub.tag != null)
-                      Row(
-                        children: [
-                          Container(
-                            width: 6,
-                            height: 6,
-                            decoration: BoxDecoration(
-                              shape: BoxShape.circle, 
-                              color: sub.tag == 'DONE' ? Colors.green : Colors.blue
-                            )
-                          ),
-                          const SizedBox(width: 4),
+                      if (sub.tag != null)
+                        Row(
+                          children: [
+                            Container(
+                              width: 6,
+                              height: 6,
+                              decoration: BoxDecoration(
+                                shape: BoxShape.circle, 
+                                color: sub.tag == 'DONE' ? Colors.green : Colors.blue
+                              )
+                            ),
+                            const SizedBox(width: 4),
+                            Text(
+                              sub.tag!,
+                              style: TextStyle(
+                                fontSize: 9,
+                                fontWeight: FontWeight.bold,
+                                color: sub.tag == 'DONE' ? Colors.green : Colors.blue
+                              ),
+                            ),
+                          ],
+                        ),
+                        if (sub.count != null)
                           Text(
-                            sub.tag!,
-                            style: TextStyle(
+                            '${sub.count}',
+                            style: const TextStyle(
                               fontSize: 9,
                               fontWeight: FontWeight.bold,
-                              color: sub.tag == 'DONE' ? Colors.green : Colors.blue
+                              color: Colors.grey,
                             ),
                           ),
-                        ],
-                      ),
-                      if (sub.count != null)
-                        Text(
-                          '${sub.count}',
-                          style: const TextStyle(
-                            fontSize: 9,
-                            fontWeight: FontWeight.bold,
-                            color: Colors.grey,
-                          ),
-                        ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
